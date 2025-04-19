d010fc66242b15f39c471d974cb12e8ead1e7b06
Alonglosef <along-losef@outlook.com>
2025-04-19 21:55:53 +0800

    update:修改版本号

--- boot.cs
+++ boot.cs
@@ -14,54 +14,56 @@ class 程序
 {
     static void Main()
     {
-        Console.WriteLine("欢迎使用LosefChat v2.0.r1.b2\n输入1 开始聊天,输入2 server,输入3 EXIT");
-
-        if (!int.TryParse(Console.ReadLine(), out int choose))
+        Console.WriteLine("欢迎使用LosefChat v2.0.r1.b12\n输入1 开始聊天,输入2 server,输入3 EXIT");
+        while (true)
         {
-            Console.WriteLine("无效输入，请输入1、2或3。");
-            return;
-        }
-
-        if (choose == 1)
-        {
-            Console.WriteLine("请选择预设文件:");
-            if (!int.TryParse(Console.ReadLine(), out int PresetChoose))
+            if (!int.TryParse(Console.ReadLine(), out int choose))
             {
-                Console.WriteLine("无效输入，请输入一个数字");
-            }
-            Client client = new Client(PresetChoose);
-
-            Preset preset = new Preset();
-            preset.ReadPreset(PresetChoose);
-
-            client.Connect(preset.ipvx, preset.ip, preset.port , preset.username, preset.password);
-        }
-        else if (choose == 2)
-        {
-            Console.Write("接下来端口号，如果您不输入,则默认选择2,如果您不是Windows系统,则默认选择9002\n请输入端口号: ");
-            if (!int.TryParse(Console.ReadLine(), out int 端口))
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    端口 = 2;
-                }
-                else
-                {
-                    端口 = 9002;
-                }
+                Console.WriteLine("无效输入，请输入1、2或3。");
+                return;
             }
 
-            Server server = new Server(端口);
+            if (choose == 1)
+            {
+                Console.WriteLine("请选择预设文件:");
+                if (!int.TryParse(Console.ReadLine(), out int PresetChoose))
+                {
+                    Console.WriteLine("无效输入，请输入一个数字");
+                }
+                Client client = new Client(PresetChoose);
 
-            server.Start();
-        }
-        else if (choose == 3)
-        {
-            Environment.Exit(0);
-        }
-        else
-        {
-            Console.WriteLine("无效输入，请输入1、2或3。");
+                Preset preset = new Preset();
+                preset.ReadPreset(PresetChoose);
+
+                client.Connect(preset.ipvx, preset.ip, preset.port, preset.username, preset.password);
+            }
+            else if (choose == 2)
+            {
+                Console.Write("接下来端口号，如果您不输入,则默认选择2,如果您不是Windows系统,则默认选择9002\n请输入端口号: ");
+                if (!int.TryParse(Console.ReadLine(), out int 端口))
+                {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        端口 = 2;
+                    }
+                    else
+                    {
+                        端口 = 9002;
+                    }
+                }
+
+                Server server = new Server(端口);
+
+                server.Start();
+            }
+            else if (choose == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("无效输入，请输入1、2或3。");
+            }
         }
     }
 }
