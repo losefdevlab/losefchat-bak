ca9c2404ce447146b8cb977fc095822a2f41b079
Alonglosef <along-losef@outlook.com>
2025-05-11 15:50:46 +0800

    update:[C]修复部分残缺问题

--- boot.cs
+++ boot.cs
@@ -74,7 +74,7 @@ class Boot
             Console.WriteLine(@" | |___  |  __/ | |_      \__ \   | |___  | | | | | (_| | | |_    | |___  | | | | | (_| | | |_  |_|");
             Console.WriteLine(@" |_____|  \___|  \__|     |___/    \____| |_| |_|  \__,_|  \__|    \____| |_| |_|  \__,_|  \__| (_)");
             Console.WriteLine("------------------------------------------------------------------------------------------------------");
-            Console.WriteLine("欢迎使用LosefChat v3.0.r3.b47\n客户端请注意:正常启动后，仅输出，输入模式请另启动程序（使用-ci附加参数启动程序）\n输入1 开始聊天,输入2 server,输入3 EXIT");
+            Console.WriteLine("欢迎使用LosefChat v3.0.r3.b53\n客户端请注意:正常启动后，仅输出，输入模式请另启动程序（使用-ci附加参数启动程序）\n输入1 开始聊天,输入2 server,输入3 EXIT");
             while (true)
             {
                 if (!int.TryParse(Console.ReadLine(), out int choose))
